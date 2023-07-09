3a7a2bc0eff858dc256c15272f65f163e2accc46
dun0077 <113908870+dun0077@users.noreply.github.com>
2023-07-09 13:09:39 +0700

    Update vmediay1.js

--- script/vmediay1.js
+++ script/vmediay1.js
@@ -1,4 +1,4 @@
- document.querySelector('#notakaki').innerHTML += ' Cr. <a href="https://t-abdul.blogspot.com" target="_blank">'+'unescape("%u0E04%u0E23%u0E39%u0E2D%u0E31%u0E1A%u0E14%u0E38%u0E25%u0E40%u0E25%u0E32%u0E30")'+'</a>'
+ document.querySelector('#notakaki').innerHTML += ' Cr. <a href="https://t-abdul.blogspot.com" target="_blank">'+unescape("%u0E04%u0E23%u0E39%u0E2D%u0E31%u0E1A%u0E14%u0E38%u0E25%u0E40%u0E25%u0E32%u0E30")+'</a>'
       function showDiscription(){
         document.querySelector('#discription').removeAttribute('style');
         document.querySelector('#btn_hid').removeAttribute('style');
